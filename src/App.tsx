c75b54a9eecfbb30cc4061c0c9e4cbd0a960b9f0
Elbembilo <Elbembilo@gmail.com>
2022-01-26 21:00:54 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,26 @@ import './App.css';
 import { Link, Routes, Route } from 'react-router-dom';
 import Basket from './pages/basket';
 import MarkItem from './components/Item/item';
+import ShowProducts from './components/Item/item';
 
 
 
 function App() {
   return (
     <>
-    <header>
-      <Link to = '/' >Home</Link>
-      <Link to = '/basket' >Basket</Link>
-      <Link to = '/item' >Item</Link>
-    </header>
-      <Routes>        
-          <Route path='/' element = {<Basket />} />
-          <Route path='/item' element = {<MarkItem />} />
-      </Routes>
+    <div className = 'container'>
+      <header className = 'header'>
+        <Link to = '/' className = 'link'>Home</Link>
+        <Link to = '/basket' className = 'link'>Basket</Link>
+        <Link to = '/item' className = 'link'>Item</Link>
+      </header>
+        <Routes>        
+            <Route path = '/' element = {<ShowProducts />} />
+            <Route path = '/item' element = {<MarkItem />} />
+            <Route path = '/basket' element = {<Basket/>} />
+        </Routes>
+    </div>
+    
     </>    
   );
 }
--- src/components/Item/item.tsx
+++ src/components/Item/item.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios'
+import { IProduct } from "../../models/products";
+import './Item.scss'
+
 
 export default function ShowProducts(){
     
-        const [products, setProducts] = useState( [] );
-        
+        const [products, setProducts] = useState <IProduct[]>( [] );
+        // const itemImg = products.img
         useEffect(() => {
             const apiUrl = 'https://fakestoreapi.com/products/'
             axios.get(apiUrl)
@@ -18,13 +21,30 @@ export default function ShowProducts(){
     
     return(
         <React.Fragment>
-            {itemData.map(allItems => (
-                <div className="body_item" key = {allItems.id}>
-                    {allItems.category}
-                </div>
+            <div className = "grid_items">
+                {products.map(product => (                
+                    <div className = "grid_item">
+                        <div className = "body_item" key = {product.id}>
+                            <div className = "item_title">
+                                {product.title}
+                            </div>
+                            <div className = "item_category">
+                                {product.category}
+                            </div>
+                            <div className = "item_price">
+                                {product.price}
+                            </div>
+                            <div className = "item_discription">
+                                {product.description}
+                            </div>
+                            <div className="item_img">
+                                <img src ={product.image}  alt={product.title} />
+                                </div>                     
+                        </div>
+                    </div>
             ))
-
             }
+            </div>
         </React.Fragment>
         
     )
